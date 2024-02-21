ca70fec54000e49c5c865ae1805c2f6d5041572d
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-02-21 12:57:00 -0300

    agregado de la pagina Resultado

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native'
 
 import HomeScreen from './src/Screens/Main'
 import ExamenNovicio from './src/Screens/ExamenNovicio'
+import Resultado from './src/Screens/Resultado'
 
 // Configuración del Stack Navigator
 const Stack = createStackNavigator();
@@ -14,6 +15,7 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}  />
         <Stack.Screen name="Novicio" component={ExamenNovicio} options={{ headerShown: false }} />
+        <Stack.Screen name="Resultado" component={Resultado} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
