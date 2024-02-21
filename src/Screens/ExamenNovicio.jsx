726c1e11b35b63188a4d53b8007516a83ff57f12
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-02-21 20:40:34 -0300

    Se agrego la carga de las preguntas de tecnica colcoando en un solo flatlist, pero separados por seccion

--- src/Screens/ExamenNovicio.jsx
+++ src/Screens/ExamenNovicio.jsx
@@ -1,117 +1,111 @@
-import React, { useState, useEffect } from 'react'
-import Constants from 'expo-constants'
-import { View, Text, StyleSheet, Pressable, FlatList } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { RadioButton } from 'react-native-paper'
+import React, { useState, useEffect } from 'react';
+import Constants from 'expo-constants';
+import { View, Text, StyleSheet, Pressable, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RadioButton } from 'react-native-paper';
 
-// Importa el archivo JSON que contiene las preguntas de novicio
-import NovicioReglamentacion from '../Data/novicioReglamentacion.json'
+// Importa los archivos JSON que contienen las preguntas de novicio
+import NovicioReglamentacion from '../Data/novicioReglamentacion.json';
+import NovicioTecnica from '../Data/novicioTecnica.json';
 
 const ExamenNovicio = () => {
-
-  const navigation = useNavigation()
-  const [respuestas, setRespuestas] = useState(Array(3).fill(''))
-  const [preguntas, setPreguntas] = useState([])
-  const [preguntasRespondidas, setPreguntasRespondidas] = useState([]) // Array de índices de preguntas respondidas
+  const navigation = useNavigation();
+  const [respuestas, setRespuestas] = useState(Array(6).fill('')); // 3 preguntas de reglamentación + 3 preguntas de técnica
+  const [preguntas, setPreguntas] = useState([]);
+  const [preguntasRespondidas, setPreguntasRespondidas] = useState([]); // Array de índices de preguntas respondidas
 
   useEffect(() => {
+    const preguntasAleatoriasReglamentacion = obtenerPreguntasAleatorias(NovicioReglamentacion, 3);
+    const preguntasAleatoriasTecnica = obtenerPreguntasAleatorias(NovicioTecnica, 3);
 
-    const preguntasAleatorias = obtenerPreguntasAleatorias()
-    setPreguntas(preguntasAleatorias)
+    const preguntasInterleaved = intercalarPreguntas(preguntasAleatoriasReglamentacion, preguntasAleatoriasTecnica);
+    setPreguntas(preguntasInterleaved);
+  }, []);
 
-  }, []) // Se ejecuta solo una vez al montar el componente
+  const obtenerPreguntasAleatorias = (preguntas, cantidad) => {
+    return preguntas.sort(() => Math.random() - 0.5).slice(0, cantidad);
+  };
 
-  // Función para manejar la selección de respuestas
+  const intercalarPreguntas = (preguntasReglamentacion, preguntasTecnica) => {
+    const preguntasInterleaved = [];
+
+    preguntasInterleaved.push({ type: 'section', title: 'Reglamentación' });
+    preguntasReglamentacion.forEach(pregunta => {
+      preguntasInterleaved.push(pregunta);
+    });
+
+    preguntasInterleaved.push({ type: 'section', title: 'Técnica' });
+    preguntasTecnica.forEach(pregunta => {
+      preguntasInterleaved.push(pregunta);
+    });
+
+    return preguntasInterleaved;
+  };
+
   const handleSeleccionRespuesta = (index, opcion) => {
+    const nuevasRespuestas = [...respuestas];
+    nuevasRespuestas[index] = opcion;
+    setRespuestas(nuevasRespuestas);
 
-    const nuevasRespuestas = [...respuestas]
-    nuevasRespuestas[index] = opcion
-    setRespuestas(nuevasRespuestas)
-
-    // Actualizar array de índices de preguntas respondidas
     if (!preguntasRespondidas.includes(index)) {
-
-      setPreguntasRespondidas([...preguntasRespondidas, index])
-
+      setPreguntasRespondidas([...preguntasRespondidas, index]);
     }
-  }
-
-  // Función para obtener preguntas aleatorias
-  const obtenerPreguntasAleatorias = () => {
-
-    return NovicioReglamentacion.sort(() => Math.random() - 0.5).slice(0, 3)
-
-  }
+  };
 
   const terminarExamen = () => {
+    navigation.navigate('Resultado', { respuestas, preguntasRespondidas });
+  };
 
-    navigation.navigate('Resultado', { respuestas, preguntasRespondidas })
-
-  }
-
-  // Renderizar componente de pregunta
-  const renderPregunta = ({ item, index }) => (
-
-    <View style={styles.preguntaContainer}>
-
-      <Text style={styles.preguntaText}>{item.numero_pregunta} - {item.pregunta}</Text>
-
-      {/* Mapea las opciones de respuesta */}
-      {item.opciones.map((opcion, idx) => (
-
-        <Pressable
-          key={idx}
-          style={styles.opcionContainer}
-          onPress={() => handleSeleccionRespuesta(index, opcion.letra)}
-        >
-          <RadioButton
-            value={opcion.letra}
-            status={respuestas[index] === opcion.letra ? 'checked' : 'unchecked'}
-            onPress={() => handleSeleccionRespuesta(index, opcion.letra)}
-          />
-          <Text>{opcion.opcion}</Text>
-        </Pressable>
-
-      ))}
-
-    </View>
-
-  )
+  const renderPregunta = ({ item, index }) => {
+    if (item.type === 'section') {
+      return (
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>{item.title}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.preguntaContainer}>
+          <Text style={styles.preguntaText}>{item.numero_pregunta} - {item.pregunta}</Text>
+          {item.opciones.map((opcion, idx) => (
+            <Pressable
+              key={idx}
+              style={styles.opcionContainer}
+              onPress={() => handleSeleccionRespuesta(index, opcion.letra)}
+            >
+              <RadioButton
+                value={opcion.letra}
+                status={respuestas[index] === opcion.letra ? 'checked' : 'unchecked'}
+                onPress={() => handleSeleccionRespuesta(index, opcion.letra)}
+              />
+              <Text>{opcion.opcion}</Text>
+            </Pressable>
+          ))}
+        </View>
+      );
+    }
+  };
 
   return (
-
     <View style={styles.container}>
-
       <Text style={styles.title}>Examen de Novicio</Text>
-
       <FlatList
         data={preguntas}
         renderItem={renderPregunta}
         keyExtractor={(item, index) => index.toString()}
         contentContainerStyle={{ paddingBottom: 20 }}
       />
-
       <View style={styles.buttonContainer}>
-
         <Pressable style={styles.button} onPress={() => navigation.navigate('Home')}>
-
           <Text style={styles.buttonText}>Atras</Text>
-
         </Pressable>
-
         <Pressable style={styles.button} onPress={terminarExamen}>
-
           <Text style={styles.buttonText}>Terminar</Text>
-
         </Pressable>
-
       </View>
-
     </View>
-
-  )
-
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -124,6 +118,12 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     textAlign: 'center',
     marginTop: Constants.statusBarHeight + 20,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginTop: 15,
   },
   preguntaContainer: {
     marginBottom: 10,
@@ -153,6 +153,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
-})
+});
 
-export default ExamenNovicio
+export default ExamenNovicio;
